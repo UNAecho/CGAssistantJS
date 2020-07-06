94c27120ba4e6c54e79ad6f24e3fde5c5a49c0ce
yad <yadhr582855555@hotmail.com>
2020-07-06 19:45:11 +0800

    快速启动与关闭批处理文件

---
+++ quickshutdown.bat
@@ -0,0 +1,5 @@
+taskkill /f /im:CGAssistant.exe
+
+taskkill /f /im:node.exe
+
+taskkill /f /im:cg_item_6000.exe
---
+++ quickstart.bat
@@ -0,0 +1,122 @@
+start CGAssistant.exe -gametype=40 -loginuser=yadhr5828555 -loginpwd=1010458058 -gid=yadhr5828555archer -server=3 -character=1 -autologin -loadscript="D:\PyCharm 2018.3.4\workspace\CGAssistantJS\通用挂机脚本.js" -scriptautorestart -loadsettings="D:\PyCharm 2018.3.4\workspace\CGAssistantJS\弓箭练级.json"
+
+timeout 30
+
+start CGAssistant.exe -gametype=40 -loginuser=yadhr5828555 -loginpwd=1010458058 -gid=yadhr5828555fighter -server=3 -character=1 -autologin -loadscript="D:\PyCharm 2018.3.4\workspace\CGAssistantJS\通用挂机脚本.js" -scriptautorestart -loadsettings="D:\PyCharm 2018.3.4\workspace\CGAssistantJS\战斗配置\营地组队普攻刷声望.json"
+
+timeout 30
+
+start CGAssistant.exe -gametype=40 -loginuser=yadhr5828555 -loginpwd=1010458058 -gid=yadhr5828555berserker -server=3 -character=1 -autologin -loadscript="D:\PyCharm 2018.3.4\workspace\CGAssistantJS\通用挂机脚本.js" -scriptautorestart -loadsettings="D:\PyCharm 2018.3.4\workspace\CGAssistantJS\战斗配置\营地组队普攻刷声望.json"
+
+timeout 30
+
+start CGAssistant.exe -gametype=40 -loginuser=yadhr5828555 -loginpwd=1010458058 -gid=yadhr5828555missionary -server=3 -character=1 -autologin -loadscript="D:\PyCharm 2018.3.4\workspace\CGAssistantJS\通用挂机脚本.js"  -loadsettings="D:\PyCharm 2018.3.4\workspace\CGAssistantJS\传教练级.json"
+
+timeout 30
+
+start CGAssistant.exe -gametype=40 -loginuser=yadhr5828555 -loginpwd=1010458058 -gid=yadhr5828555caster -server=3 -character=1 -autologin -loadscript="D:\PyCharm 2018.3.4\workspace\CGAssistantJS\通用挂机脚本.js" -scriptautorestart -loadsettings="D:\PyCharm 2018.3.4\workspace\CGAssistantJS\战斗配置\营地组队普攻刷声望.json"
+
+timeout 30
+
+start CGAssistant.exe -gametype=40 -loginuser=yadhr5828555 -loginpwd=1010458058 -gid=yadhr5828555hunter1 -server=3 -character=1 -autologin -loadscript="D:\PyCharm 2018.3.4\workspace\CGAssistantJS\通用挂机脚本.js" -scriptautorestart -loadsettings="D:\PyCharm 2018.3.4\workspace\CGAssistantJS\生产赶路不登出.json"
+
+timeout 30
+
+start CGAssistant.exe -gametype=40 -loginuser=yadhr5828555 -loginpwd=1010458058 -gid=yadhr5828555hunter2 -server=3 -character=1 -autologin -loadscript="D:\PyCharm 2018.3.4\workspace\CGAssistantJS\通用挂机脚本.js" -scriptautorestart -loadsettings="D:\PyCharm 2018.3.4\workspace\CGAssistantJS\生产赶路不登出.json"
+
+timeout 30
+
+start CGAssistant.exe -gametype=40 -loginuser=yadhr5828555 -loginpwd=1010458058 -gid=yadhr5828555hunter3 -server=3 -character=1 -autologin -loadscript="D:\PyCharm 2018.3.4\workspace\CGAssistantJS\通用挂机脚本.js" -scriptautorestart -loadsettings="D:\PyCharm 2018.3.4\workspace\CGAssistantJS\生产赶路不登出.json"
+
+timeout 30
+
+start CGAssistant.exe -gametype=40 -loginuser=yadhr5828555 -loginpwd=1010458058 -gid=yadhr5828555hunter4 -server=3 -character=1 -autologin -loadscript="D:\PyCharm 2018.3.4\workspace\CGAssistantJS\通用挂机脚本.js" -scriptautorestart -loadsettings="D:\PyCharm 2018.3.4\workspace\CGAssistantJS\生产赶路不登出.json"
+
+timeout 30
+
+start CGAssistant.exe -gametype=40 -loginuser=yadhr5828555 -loginpwd=1010458058 -gid=yadhr5828555hunter5 -server=3 -character=1 -autologin -loadscript="D:\PyCharm 2018.3.4\workspace\CGAssistantJS\通用挂机脚本.js" -scriptautorestart -loadsettings="D:\PyCharm 2018.3.4\workspace\CGAssistantJS\生产赶路不登出.json"
+
+timeout 30
+
+start CGAssistant.exe -gametype=40 -loginuser=yadhr5828555 -loginpwd=1010458058 -gid=yadhr5828555chef -server=3 -character=1 -autologin -loadscript="D:\PyCharm 2018.3.4\workspace\CGAssistantJS\通用挂机脚本.js" -scriptautorestart -loadsettings="D:\PyCharm 2018.3.4\workspace\CGAssistantJS\生产赶路.json"
+
+timeout 30
+
+start CGAssistant.exe -gametype=40 -loginuser=yadhr5828555 -loginpwd=1010458058 -gid=yadhr5828555woodcutter1 -server=3 -character=1 -autologin -loadscript="D:\PyCharm 2018.3.4\workspace\CGAssistantJS\通用挂机脚本.js" -scriptautorestart -loadsettings="D:\PyCharm 2018.3.4\workspace\CGAssistantJS\生产赶路不登出.json"
+
+timeout 30
+
+start CGAssistant.exe -gametype=40 -loginuser=yadhr5828555 -loginpwd=1010458058 -gid=yadhr5828555woodcutter2 -server=3 -character=1 -autologin -loadscript="D:\PyCharm 2018.3.4\workspace\CGAssistantJS\通用挂机脚本.js" -scriptautorestart -loadsettings="D:\PyCharm 2018.3.4\workspace\CGAssistantJS\生产赶路不登出.json"
+
+timeout 30
+
+start CGAssistant.exe -gametype=40 -loginuser=yadhr5828555 -loginpwd=1010458058 -gid=yadhr5828555woodcutter3 -server=3 -character=1 -autologin -loadscript="D:\PyCharm 2018.3.4\workspace\CGAssistantJS\通用挂机脚本.js" -scriptautorestart -loadsettings="D:\PyCharm 2018.3.4\workspace\CGAssistantJS\生产赶路不登出.json"
+
+timeout 30
+
+start CGAssistant.exe -gametype=40 -loginuser=yadhrmanufactory -loginpwd=1010458058 -gid=yadhrmanufactoryminer1 -server=3 -character=1 -autologin -loadscript="D:\PyCharm 2018.3.4\workspace\CGAssistantJS\通用挂机脚本.js" -scriptautorestart -loadsettings="D:\PyCharm 2018.3.4\workspace\CGAssistantJS\生产赶路不登出.json"
+
+timeout 30
+
+start CGAssistant.exe -gametype=40 -loginuser=yadhrmanufactory -loginpwd=1010458058 -gid=yadhrmanufactoryminer2 -server=3 -character=1 -autologin -loadscript="D:\PyCharm 2018.3.4\workspace\CGAssistantJS\通用挂机脚本.js" -scriptautorestart -loadsettings="D:\PyCharm 2018.3.4\workspace\CGAssistantJS\生产赶路不登出.json"
+
+timeout 30
+
+start CGAssistant.exe -gametype=40 -loginuser=yadhrmanufactory -loginpwd=1010458058 -gid=yadhrmanufactoryminer3 -server=3 -character=1 -autologin -loadscript="D:\PyCharm 2018.3.4\workspace\CGAssistantJS\通用挂机脚本.js" -scriptautorestart -loadsettings="D:\PyCharm 2018.3.4\workspace\CGAssistantJS\生产赶路不登出.json"
+
+timeout 30
+
+start CGAssistant.exe -gametype=40 -loginuser=yadhrmanufactory -loginpwd=1010458058 -gid=yadhrmanufactoryminer4 -server=3 -character=1 -autologin -loadscript="D:\PyCharm 2018.3.4\workspace\CGAssistantJS\通用挂机脚本.js"  -scriptautorestart -loadsettings="D:\PyCharm 2018.3.4\workspace\CGAssistantJS\生产赶路不登出.json"
+
+timeout 30
+
+start CGAssistant.exe -gametype=40 -loginuser=yadhrmanufactory -loginpwd=1010458058 -gid=yadhrmanufactoryminer5 -server=3 -character=1 -autologin -loadscript="D:\PyCharm 2018.3.4\workspace\CGAssistantJS\通用挂机脚本.js" -scriptautorestart -loadsettings="D:\PyCharm 2018.3.4\workspace\CGAssistantJS\生产赶路不登出.json"
+
+timeout 30
+
+start CGAssistant.exe -gametype=40 -loginuser=yadhrmanufactory -loginpwd=1010458058 -gid=yadhrmanufactoryaxe -server=3 -character=1 -autologin -loadscript="D:\PyCharm 2018.3.4\workspace\CGAssistantJS\通用挂机脚本.js"  -scriptautorestart -loadsettings="D:\PyCharm 2018.3.4\workspace\CGAssistantJS\战斗配置\东门烧声望传.json"
+
+timeout 30
+
+start CGAssistant.exe -gametype=40 -loginuser=yadhrmanufactory -loginpwd=1010458058 -gid=yadhrmanufactorybow -server=3 -character=1 -autologin -loadscript="D:\PyCharm 2018.3.4\workspace\CGAssistantJS\通用挂机脚本.js"  -scriptautorestart -loadsettings="D:\PyCharm 2018.3.4\workspace\CGAssistantJS\生产赶路.json"
+
+timeout 30
+
+start CGAssistant.exe -gametype=40 -loginuser=yadhrbattle -loginpwd=1010458058 -gid=yadhrbattlenurse -server=3 -character=1 -autologin -loadscript="D:\PyCharm 2018.3.4\workspace\CGAssistantJS\飞碟摆摊治疗.js"  -scriptautorestart -loadsettings="D:\PyCharm 2018.3.4\workspace\CGAssistantJS\战斗配置\东门烧声望传.json"
+
+timeout 30
+
+start CGAssistant.exe -gametype=40 -loginuser=yadhrbattle -loginpwd=1010458058 -gid=yadhrbattlesealer -server=3 -character=1 -autologin -loadscript="D:\PyCharm 2018.3.4\workspace\CGAssistantJS\通用挂机脚本.js"  -scriptautorestart -loadsettings="D:\PyCharm 2018.3.4\workspace\CGAssistantJS\战斗配置\东门烧声望咒.json"
+
+timeout 30
+
+start CGAssistant.exe -gametype=40 -loginuser=yadhrmanufactory -loginpwd=1010458058 -gid=yadhrmanufactoryknife -server=3 -character=1 -autologin -loadscript="D:\PyCharm 2018.3.4\workspace\CGAssistantJS\通用挂机脚本.js"  -scriptautorestart -loadsettings="D:\PyCharm 2018.3.4\workspace\CGAssistantJS\生产赶路.json"
+
+timeout 30
+
+
+start CGAssistant.exe -gametype=40 -loginuser=yadhrbattle -loginpwd=1010458058 -gid=yadhrpugilist2 -server=3 -character=1 -autologin -loadscript="D:\PyCharm 2018.3.4\workspace\CGAssistantJS\通用挂机脚本.js"  -scriptautorestart -loadsettings="D:\PyCharm 2018.3.4\workspace\CGAssistantJS\战斗配置\东门烧声望咒.json"
+
+timeout 30
+
+goto temp
+start CGAssistant.exe -gametype=40 -loginuser=yadhr5828555 -loginpwd=1010458058 -gid=yadhr5828555pharmacist -server=3 -character=2 -autologin -loadscript="D:\PyCharm 2018.3.4\workspace\CGAssistantJS\通用挂机脚本.js" -scriptautorestart -loadsettings="D:\PyCharm 2018.3.4\workspace\CGAssistantJS\生产赶路.json"
+
+timeout 30
+
+start CGAssistant.exe -gametype=40 -loginuser=yadhrbattle -loginpwd=1010458058 -gid=yadhrbattlewitch -server=3 -character=1 -autologin -loadscript="D:\PyCharm 2018.3.4\workspace\CGAssistantJS\通用挂机脚本.js"  -scriptautorestart -loadsettings="D:\PyCharm 2018.3.4\workspace\CGAssistantJS\生产练级.json"
+
+timeout 30
+
+start CGAssistant.exe -gametype=40 -loginuser=yadhr5828555 -loginpwd=1010458058 -gid=yadhr5828555sealer -server=3 -character=1 -autologin -loadscript="D:\PyCharm 2018.3.4\workspace\CGAssistantJS\通用挂机脚本.js" -scriptautorestart -loadsettings="D:\PyCharm 2018.3.4\workspace\CGAssistantJS\生产赶路.json"
+
+timeout 30
+:temp
+goto temp2
+
+:temp2
+
+
+start CGAssistant.exe -gametype=40 -loginuser=yadhr5828555 -loginpwd=1010458058 -gid=yadhr5828555appraiser -server=3 -character=1 -autologin -loadscript="D:\PyCharm 2018.3.4\workspace\CGAssistantJS\通用挂机脚本.js" -scriptautorestart -loadsettings="D:\PyCharm 2018.3.4\workspace\CGAssistantJS\生产赶路不登出.json"
+
+timeout 30
