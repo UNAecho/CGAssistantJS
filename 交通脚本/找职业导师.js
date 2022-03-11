ed9461547a79db96425caef95d2bedc53148827c
yad <yadhr582855555@hotmail.com>
2022-03-11 21:22:34 +0800

    找职业导师添加临时输入功能

--- 交通脚本/找职业导师.js
+++ 交通脚本/找职业导师.js
@@ -12,11 +12,11 @@ var cga = require('../cgaapi')(function(){
 			}
 		});
 	}else{
-		var sayString = '您想做什么？ 请输入编号\n[0]找职业导师晋级，[1]找本职技能NPC学技能，[2]找职业导师就职，[3]找职业导师转职。';
+		var sayString = '您想做什么？ 请输入编号\n[0]找职业导师晋级，[1]找本职技能NPC学技能，[2]找职业导师就职，[3]找职业导师转职。如果想找去的职业导师所在地，请输入职业名称+$。如：格斗士$。';
 		cga.sayLongWords(sayString, 0, 3, 1);
 		cga.waitForChatInput((msg, val)=>{
-			if(val !== null && val >= 0 && val <= 3){
-				
+			var val = parseInt(msg);
+			if(val !== NaN && val >= 0 && val <= 3){
 				switch(val){
 					case 0:
 						behavior = 'promote'
@@ -36,6 +36,10 @@ var cga = require('../cgaapi')(function(){
 				}
 				getProfessionalbehavior(cga,job,behavior)
 				return false;
+			}
+			else if(msg.length>0 && msg.charAt(msg.length - 1) == '$'){// 临时默认找导师就职 TODO将所有职业列出来并编号，让玩家输入数字选择
+				var inputjob = msg.substring(0, msg.length - 1);
+				getProfessionalbehavior(cga,inputjob,'induction')
 			}
 			
 			return true;
