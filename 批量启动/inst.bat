bafe2b0fb57f9b0d3bdd86564dd0362a2bca37b9
yad <yadhr582855555@hotmail.com>
2022-03-20 14:49:23 +0800

    更新

---
+++ 批量启动/inst.bat
@@ -0,0 +1,31 @@
+start ../CGAssistant.exe -gametype=40 -loginuser= -loginpwd= -gid= -server=3 -character=1 -autologin -skipupdate -loadscript="H:\WorkSpace\CGAssistantJS\通用挂机脚本.js"  -scriptautorestart -loadsettings="H:\WorkSpace\CGAssistantJS\战斗配置\格斗练级.json"
+
+timeout 30
+
+start ../CGAssistant.exe -gametype=40 -loginuser= -loginpwd= -gid= -server=3 -character=1 -autologin  -skipupdate -loadscript="H:\WorkSpace\CGAssistantJS\通用挂机脚本.js"  -scriptautorestart -loadsettings="H:\WorkSpace\CGAssistantJS\战斗配置\格斗练级.json"
+
+timeout 30
+
+start ../CGAssistant.exe -gametype=40 -loginuser= -loginpwd= -gid= -server=3 -character=1 -autologin  -skipupdate -loadscript="H:\WorkSpace\CGAssistantJS\通用挂机脚本.js"  -scriptautorestart -loadsettings="H:\WorkSpace\CGAssistantJS\战斗配置\生产练级.json"
+
+timeout 30
+
+start ../CGAssistant.exe -gametype=40 -loginuser= -loginpwd= -gid= -server=3 -character=1 -autologin  -skipupdate -loadscript="H:\WorkSpace\CGAssistantJS\通用挂机脚本.js"  -scriptautorestart -loadsettings="H:\WorkSpace\CGAssistantJS\战斗配置\生产练级.json"
+
+timeout 30
+
+start ../CGAssistant.exe -gametype=40 -loginuser= -loginpwd= -gid= -server=3 -character=1 -autologin -skipupdate -loadscript="H:\WorkSpace\CGAssistantJS\通用挂机脚本.js"  -scriptautorestart -loadsettings="H:\WorkSpace\CGAssistantJS\战斗配置\传教练级.json"
+
+timeout 30
+
+start ../CGAssistant.exe -gametype=40 -loginuser= -loginpwd= -gid= -server=3 -character=1 -autologin  -skipupdate -loadscript="H:\WorkSpace\CGAssistantJS\leo\封印--水龙蜥(自动战斗+自动算档+消息服务).js"  -scriptautorestart -loadsettings="H:\WorkSpace\CGAssistantJS\leo\~抓宠(1火2抓) 修正.json"
+
+timeout 30
+
+start ../CGAssistant.exe -gametype=40 -loginuser= -loginpwd= -gid= -server=3 -character=1 -autologin -skipupdate -loadscript="H:\WorkSpace\CGAssistantJS\飞碟摆摊治疗.js"  -scriptautorestart -loadsettings="H:\WorkSpace\CGAssistantJS\战斗配置\生产赶路.json"
+
+timeout 30
+
+start ../CGAssistant.exe -gametype=40 -loginuser= -loginpwd= -gid= -server=3 -character=1 -autologin -skipupdate -loadscript="H:\WorkSpace\CGAssistantJS\通用挂机脚本.js"  -scriptautorestart -loadsettings="H:\WorkSpace\CGAssistantJS\战斗配置\生产赶路.json"
+
+timeout 30
---
+++ 批量启动/reputation.bat
@@ -0,0 +1,27 @@
+start ../CGAssistant.exe -gametype=40 -loginuser= -loginpwd= -gid= -server=3 -character=1 -autologin -skipupdate -loadscript="H:\WorkSpace\CGAssistantJS\转职保证书(卵4).js"  -scriptautorestart -loadsettings="H:\WorkSpace\CGAssistantJS\战斗配置\BOSS合击.json"
+
+timeout 30
+
+start ../CGAssistant.exe -gametype=40 -loginuser= -loginpwd= -gid= -server=3 -character=1 -autologin  -skipupdate -loadscript="H:\WorkSpace\CGAssistantJS\转职保证书(卵4).js"  -scriptautorestart -loadsettings="H:\WorkSpace\CGAssistantJS\战斗配置\BOSS合击.json"
+
+timeout 30
+
+start ../CGAssistant.exe -gametype=40 -loginuser= -loginpwd= -gid= -server=3 -character=1 -autologin  -skipupdate -loadscript="H:\WorkSpace\CGAssistantJS\转职保证书(卵4).js"  -scriptautorestart -loadsettings="H:\WorkSpace\CGAssistantJS\战斗配置\BOSS合击.json"
+
+timeout 30
+
+start ../CGAssistant.exe -gametype=40 -loginuser= -loginpwd= -gid= -server=3 -character=1 -autologin  -skipupdate -loadscript="H:\WorkSpace\CGAssistantJS\转职保证书(卵4).js"  -scriptautorestart -loadsettings="H:\WorkSpace\CGAssistantJS\战斗配置\BOSS合击.json"
+
+timeout 30
+
+start ../CGAssistant.exe -gametype=40 -loginuser= -loginpwd= -gid= -server=3 -character=1 -autologin -skipupdate -loadscript="H:\WorkSpace\CGAssistantJS\转职保证书(卵4).js"  -scriptautorestart -loadsettings="H:\WorkSpace\CGAssistantJS\战斗配置\BOSS传教.json"
+
+timeout 30
+
+start ../CGAssistant.exe -gametype=40 -loginuser= -loginpwd= -gid= -server=3 -character=1 -autologin -skipupdate -loadscript="H:\WorkSpace\CGAssistantJS\飞碟摆摊治疗.js"  -scriptautorestart -loadsettings="H:\WorkSpace\CGAssistantJS\战斗配置\生产赶路.json"
+
+timeout 30
+
+start ../CGAssistant.exe -gametype=40 -loginuser= -loginpwd= -gid= -server=3 -character=1 -autologin -skipupdate -loadscript="H:\WorkSpace\CGAssistantJS\通用挂机脚本.js"  -scriptautorestart -loadsettings="H:\WorkSpace\CGAssistantJS\战斗配置\生产赶路.json"
+
+timeout 30
