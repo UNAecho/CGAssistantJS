4489fb34dc0020ccade056d4216345acddbf4648
yad <yadhr582855555@hotmail.com>
2022-05-27 19:55:09 +0800

    小更新

--- 交通脚本/找职业导师.js
+++ 交通脚本/找职业导师.js
@@ -12,7 +12,7 @@ var cga = require('../cgaapi')(function(){
 			}
 		});
 	}else{
-		var sayString = '您想做什么？ 请输入编号\n[0]找职业导师晋级，[1]找本职技能NPC学技能，[2]找职业导师就职，[3]找职业导师转职。如果想找去的职业导师所在地，请输入职业名称+$。如：格斗士$。';
+		var sayString = '【UNA脚本】您想做什么？ 请输入编号\n[0]找职业导师晋级，[1]找本职技能NPC学技能，[2]找职业导师就职，[3]找职业导师转职。如果想找去的职业导师所在地，请输入职业名称+$。如：格斗士$。';
 		cga.sayLongWords(sayString, 0, 3, 1);
 		cga.waitForChatInput((msg, val)=>{
 			var val = parseInt(msg);
