b0569672082a0439b5be698adbf7be09fbb8fa23
UNAecho <yadhr582855555@hotmail.com>
2022-06-24 18:07:51 +0800

    新出生快速启动

--- 批量启动/new.bat
+++ 批量启动/new.bat
@@ -1,3 +1,3 @@
-start ../CGAssistant.exe -gametype=40 -loginuser= -loginpwd= -gid=yadhrbattlepugilist3 -server=3 -character=1 -autologin -loadscript="H:\WorkSpace\CGAssistantJS\测试出生.js"  -scriptautorestart -loadsettings="H:\WorkSpace\CGAssistantJS\战斗配置\生产赶路.json" -killfreeze=60 -autocreatechara -createcharaname="UNAの格斗3" -createcharachara=3 -createcharacolor=2 -createcharapoints="15,0,0,15,0" -createcharaelements="10,0,0,0" 
+start ../CGAssistant.exe -gametype=40 -loginuser= -loginpwd= -gid= -server=3 -character=1 -autologin -loadscript="../测试出生.js"  -scriptautorestart -loadsettings="../战斗配置/生产赶路.json"
 
 timeout 30
