1b019cbbf1de9daffd24069d9e0631fa24fbfe8d
yad <yadhr582855555@hotmail.com>
2022-08-18 18:03:42 +0800

    批量启动更新

--- 批量启动/temp.bat
+++ 批量启动/temp.bat
@@ -1,3 +1,3 @@
-start ../CGAssistant.exe -gametype=40 -loginuser= -loginpwd= -gid=yadhrbattlesealer5 -server=3 -character=1 -autologin -loadscript="D:\WorkSpace\CGAssistantJS\通用挂机脚本.js"  -scriptautorestart -loadsettings="D:\WorkSpace\CGAssistantJS\战斗配置\回力镖魔宠练级.json"
+start ../CGAssistant.exe -gametype=40 -loginuser= -loginpwd= -gid= -server=3 -character=1 -autologin -skipupdate -loadscript="../通用挂机脚本.js"  -scriptautorestart -loadsettings="../战斗配置/生产赶路.json"
 
 timeout 30
