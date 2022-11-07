bb2b69059f26ec76b32d7b0392867ce3ce53a5c5
yad <yadhr582855555@hotmail.com>
2022-11-07 17:05:44 +0800

    添加人物快速新建的启动bat

--- 批量启动/new.bat
+++ 批量启动/new.bat
@@ -1,3 +1,3 @@
-start ../CGAssistant.exe -gametype=40 -loginuser= -loginpwd= -gid= -server=3 -character=1 -autologin -loadscript="../测试出生.js"  -scriptautorestart -loadsettings="../战斗配置/生产赶路.json"
+start ../CGAssistant.exe -gametype=40 -loginuser= -loginpwd= -gid= -server=3 -character=1 -autologin -loadscript="../测试出生.js" -autocreatechara -createcharaname="UNAの" -createcharachara=3 -createcharacolor=2 -createcharapoints="15,0,0,15,0" -createcharaelements="0,0,5,5" -scriptautorestart -loadsettings="../战斗配置/生产赶路.json"
 
 timeout 30
