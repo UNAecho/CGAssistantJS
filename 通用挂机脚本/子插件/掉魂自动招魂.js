a21760cf8dc3d18ee2ae36ba73ac8e002833e96c
yad <yadhr582855555@hotmail.com>
2022-11-09 18:15:19 +0800

    掉魂自动招魂添加哥拉尔逻辑

--- 通用挂机脚本/子插件/掉魂自动招魂.js
+++ 通用挂机脚本/子插件/掉魂自动招魂.js
@@ -13,34 +13,79 @@ var thisobj = {
 			setTimeout(thisobj.prepare, 1000, cb);
 			return;
 		}
-		
-		cga.travel.falan.toStone('C', ()=>{
-			cga.walkList([
-			[41, 14, '法兰城'],
-			[153, 29, '大圣堂的入口'],
-			[14, 7, '礼拜堂'],
-			[12, 19],
-			], ()=>{
-				cga.TurnTo(12, 17);
-				cga.AsyncWaitNPCDialog((err, dlg)=>{
-					if(dlg.message && dlg.message.indexOf('我将为你取回') >= 0)
-					{
-						cga.ClickNPCDialog(4, 0);
-						cga.AsyncWaitNPCDialog((err, dlg)=>{
-							if(dlg.message && dlg.message.indexOf('不够') >= 0){
-								throw new Error('招魂钱不够！');
-							}
-							
-							cb(null);
-						});
-					}
-					else
-					{
+		// UNAecho:添加在国外招魂的逻辑
+		console.log('【注意】检测到人物掉魂，去招魂。')
+
+		var villageName = cga.travel.switchMainMap()
+
+		var tmplist = []
+		var tmpTurnDir = null
+
+		var talkToNPC = (tmpTurnDir)=>{
+			cga.turnDir(tmpTurnDir)
+			cga.AsyncWaitNPCDialog((err, dlg)=>{
+				if(dlg.message && dlg.message.indexOf('我将为你取回') >= 0)
+				{
+					cga.ClickNPCDialog(4, 0);
+					cga.AsyncWaitNPCDialog((err, dlg)=>{
+						if(dlg.message && dlg.message.indexOf('不够') >= 0){
+							throw new Error('招魂钱不够！');
+						}
+						
 						cb(null);
-					}
+					});
+				}
+				else
+				{
+					cb(null);
+				}
+			});
+		}
+
+		if(villageName == '哥拉尔镇'){
+			tmplist.push(
+				[160, 199, '白之宫殿'],
+				[47, 36, 43210],
+				[61, 46],
+				)
+			tmpTurnDir = 0
+
+			cga.travel.autopilot('主地图',()=>{
+				cga.walkList(tmplist, ()=>{
+					talkToNPC(tmpTurnDir)
+				});
+			})
+			return
+		}else if(villageName == '阿凯鲁法村'){
+			// TODO
+			// tmplist.push(
+			// 	[160, 199, '白之宫殿'],
+			// 	[47, 36, 43210],
+			// 	[61, 46],
+			// 	)
+			// tmpTurnDir = 0
+
+			// cga.travel.autopilot('主地图',()=>{
+			// 	cga.walkList(tmplist, ()=>{
+			// 		talkToNPC(tmpTurnDir)
+			// 	});
+			// })
+			// return
+		}else{
+			tmpTurnDir = 6
+
+			cga.travel.falan.toStone('C', ()=>{
+				cga.walkList([
+				[41, 14, '法兰城'],
+				[153, 29, '大圣堂的入口'],
+				[14, 7, '礼拜堂'],
+				[12, 19],
+				], ()=>{
+					talkToNPC(tmpTurnDir)
 				});
 			});
-		});
+			return
+		}
 	},
 	think : (ctx)=>{
 		if(cga.GetPlayerInfo().souls != 0)
